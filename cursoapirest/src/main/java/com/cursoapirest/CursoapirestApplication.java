786f2627d0c0a9218e8e45c88334e941b27cc242
Valdeci Martins Licar <valmarlic@yahoo.com.br>
2023-03-21 11:31:05 -0300

    Git rest spring intlij
    Teste git com Api rest Spring boot intelij

---
+++ cursoapirest/src/main/java/com/cursoapirest/CursoapirestApplication.java
@@ -0,0 +1,13 @@
+package com.cursoapirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoapirestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoapirestApplication.class, args);
+	}
+
+}
---
+++ cursoapirest/src/test/java/com/cursoapirest/CursoapirestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cursoapirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoapirestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
